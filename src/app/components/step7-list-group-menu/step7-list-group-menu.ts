8afb8b4d7bfe2f52d281197017433073c82ca6b1
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-07-14 01:11:16 +0300

    chore: add explanation text to navbar

--- src/app/components/step7-list-group-menu/step7-list-group-menu.ts
+++ src/app/components/step7-list-group-menu/step7-list-group-menu.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-step7-list-group-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './step7-list-group-menu.html',
   styleUrl: './step7-list-group-menu.css',
 })
-export class Step7ListGroupMenu {}
+export class Step7ListGroupMenu {
+  menu = [
+    { text: "Step2 Person Table", appLink:'person-table-example'},
+    { text: "Step3 Component Input", appLink:'component-input-example'},
+    { text: "Step4 @for Directive", appLink:'for-directive-example'},
+    { text: "Step5 Event Bind", appLink:'event-bind-example'},
+    { text: "Step8 Simple Datatable", appLink:'simple-datatable-example'},
+    { text: "Step9 Output Example", appLink: 'component-output-example'},
+    { text: "Step10 Template Forms", appLink:'template-driven-form-example'},
+    { text: "Step11 Reactive Forms", appLink:'reactive-form-example'},
+    { text: "Step12 Http Client", appLink: 'http-client-example'},
+    { text: "Step13 User Login", appLink:'user-login'},
+    { text: "Step14 User Registration", appLink: 'user-registration-example'},
+    { text: "Restricted Content", appLink:'restricted-content'}
+  ]
+}
