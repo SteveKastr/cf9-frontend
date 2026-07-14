036bf79bee38640e068c3650e29c7a7bd61fe009
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-07-15 02:28:11 +0300

    feat: add interceptor service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,20 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-// Added from us for http request
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+// Added from us for http requests
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AuthInterceptorService } from './shared/services/auth-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
   ]
 };
