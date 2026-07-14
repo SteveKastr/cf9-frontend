76dacca64f715edecf112f04b56222d1c424f659
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-07-14 17:29:21 +0300

    feat: add http client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// Added from us for http request
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi())
   ]
 };
